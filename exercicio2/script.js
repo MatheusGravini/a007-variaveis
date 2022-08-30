577bebaef2e7fd29bda74409394d2e63142f6347
MatheusGravini <gravini.mello@gmail.com>
2022-08-29 22:27:04 -0300

    Exercícios de Variáveis

---
+++ exercicio2/script.js
@@ -0,0 +1,23 @@
+
+//a)
+let perguntaEstudo = "Você já estudou hoje?" 
+let respostaEstudo = prompt(perguntaEstudo);
+
+let perguntaComida = "Você está com fome?"
+let respostaComida = prompt(perguntaComida);
+
+let perguntaSono = "Você está com sono?"
+let respostaSono = prompt(perguntaSono);
+
+//b)
+console.log (respostaEstudo)
+console.log (respostaComida)
+console.log (respostaSono)
+
+//c) R: Minhas perguntas já começaram sendo uma variável
+
+//d)
+console.log ((perguntaEstudo), (respostaEstudo))
+console.log ((perguntaComida), (respostaComida))
+console.log ((perguntaSono), (respostaSono))
+
